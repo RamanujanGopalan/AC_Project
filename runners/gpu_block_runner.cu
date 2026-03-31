f9dbd00399306ceba179f781c42f3bd8c081ff60
Saksham <saksham23466@iiitd.ac.in>
2026-03-31 22:28:05 +0530

    update gpu runner

--- runners/gpu_block_runner.cu
+++ runners/gpu_block_runner.cu
@@ -6,10 +6,6 @@
 #include "../gpu/modes/block_ctr_gpu.cuh"
 #include "../gpu/modes/block_ecb_gpu.cuh"
 
-#include "../include/cipher_descriptor.hpp"
-#include "../include/gpu_cipher_api.cuh"
-
-#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <string>
@@ -23,7 +19,7 @@ void print_usage(const char *program) {
     std::printf("Modes: ecb, ctr\n");
 }
 
-}// namespace
+} // namespace
 
 int main(int argc, char **argv) {
     if (argc != 4) {
@@ -31,42 +27,39 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    const std::string cipher = argv[1];
+    const std::string cipher_name = argv[1];
     const std::string mode = argv[2];
     const size_t blocks = static_cast<size_t>(std::strtoull(argv[3], nullptr, 10));
 
-    const GpuBlockCipherApi *cipher = get_gpu_block_cipher(cipher_name.c_str());
-    if (cipher == nullptr) {
-        std::fprintf(stderr, "unsupported gpu block cipher: %s\n", cipher_name.c_str());
-    if (cipher == "aes") {
+    if (cipher_name == "aes") {
         if (mode == "ecb") {
             return run_gpu_block_ecb_mode(aes128_gpu::descriptor, blocks, aes128_gpu::launch_ecb);
         }
         if (mode == "ctr") {
             return run_gpu_block_ctr_mode(aes128_gpu::descriptor, blocks, 0xABCDEF1234567890ULL, aes128_gpu::launch_ctr);
         }
-    } else if (cipher == "des") {
+    } else if (cipher_name == "des") {
         if (mode == "ecb") {
             return run_gpu_block_ecb_mode(des_gpu::descriptor, blocks, des_gpu::launch_ecb);
         }
         if (mode == "ctr") {
             return run_gpu_block_ctr_mode(des_gpu::descriptor, blocks, 0xABCDEF1234567890ULL, des_gpu::launch_ctr);
         }
-    } else if (cipher == "gift") {
+    } else if (cipher_name == "gift") {
         if (mode == "ecb") {
             return run_gpu_block_ecb_mode(gift128_gpu::descriptor, blocks, gift128_gpu::launch_ecb);
         }
         if (mode == "ctr") {
             return run_gpu_block_ctr_mode(gift128_gpu::descriptor, blocks, 0xABCDEF1234567890ULL, gift128_gpu::launch_ctr);
         }
-    } else if (cipher == "kalyna") {
+    } else if (cipher_name == "kalyna") {
         if (mode == "ecb") {
             return run_gpu_block_ecb_mode(kalyna128_gpu::descriptor, blocks, kalyna128_gpu::launch_ecb);
         }
         if (mode == "ctr") {
             return run_gpu_block_ctr_mode(kalyna128_gpu::descriptor, blocks, 0xABCDEF1234567890ULL, kalyna128_gpu::launch_ctr);
         }
-    } else if (cipher == "simon") {
+    } else if (cipher_name == "simon") {
         if (mode == "ecb") {
             return run_gpu_block_ecb_mode(simon128_gpu::descriptor, blocks, simon128_gpu::launch_ecb);
         }
@@ -74,11 +67,10 @@ int main(int argc, char **argv) {
             return run_gpu_block_ctr_mode(simon128_gpu::descriptor, blocks, 0xABCDEF1234567890ULL, simon128_gpu::launch_ctr);
         }
     } else {
-        std::fprintf(stderr, "unsupported gpu block cipher: %s\n", cipher.c_str());
+        std::fprintf(stderr, "unsupported gpu block cipher: %s\n", cipher_name.c_str());
         return 1;
     }
 
     std::fprintf(stderr, "unsupported mode: %s\n", mode.c_str());
     return 1;
-    }
 }
